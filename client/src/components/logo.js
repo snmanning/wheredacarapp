6febb41a15b36be7a4c4842ab4af76f4036df194
Sarah Manning <sarahnmanning@gmail.com>
2018-08-09 19:37:47 -0400

    bare bones structure of app components map, mapcontainer and logo

---
+++ client/src/components/logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    render() {
+        return(
+            <div>
+                <h1>WHEREDACAR@APP LOGO</h1>
+            </div>
+        );
+    }
+}
+
+export defaults Logo;
---
+++ client/src/components/mapContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Map from './Map';
+
+class MapContainer extends Component {
+    render() {
+        return(
+            <div>
+                <Map/>
+            </div>
+        );
+    }
+}
+
+export default MapContainer;
